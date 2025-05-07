9d5e0fb8b6f7f97babdeb563f944bda1c836a49f
fromanoel <fernanda.romanoel@estudante.iftm.edu.br>
2025-05-07 11:43:38 -0300

    chore: adjusted cookies config

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -4,15 +4,13 @@ export const cookieConfig = {
       secure: true, 
       sameSite: 'None',
       maxAge: 15 * 60 * 1000, //15 minutos
-      domain: 'localhost', 
       path: "/",
     },
     refreshToken: {
       httpOnly: true,
-      secure: 'true', // Apenas em produção
+      secure: true, // Apenas em produção
       sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias
-      domain: 'localhost',
       path: "/",
     },
   };
